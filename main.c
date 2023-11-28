2038b4c1de2d0b222bf92199be7c017e976096e5
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2023-11-28 00:13:52 -0500

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 #define UP 0
 #define DOWN 1
@@ -12,17 +11,14 @@
 #define BUFSIZE 256
 #define CONSTANT 128
 
-double begin = 0;
-
 typedef struct Entry {
-  char *word;
+  int *arr;
   struct Entry *next;
 } Entry;
 
 typedef struct Node {
   int *board;
   int size;
-  char *board_str;
   struct Node *parent;
   int move;
 } Node;
@@ -79,15 +75,15 @@ Queue *init_queue(int size) {
   return queue;
 }
 
-size_t hash(char *word, size_t size) {
+size_t hash(int *arr, int arr_len, size_t size) {
+  const size_t prime = 31;
+  size_t hash = 0;
 
-  size_t index = 0;
-  int idx = 0;
-  while (word[idx] != '\0') {
-    index = (index + word[idx]) * 101;
-    idx++;
+  for (size_t i = 0; i < arr_len; i++) {
+    hash = hash * prime + (size_t)arr[i];
   }
-  return (size_t)(index * CONSTANT) % size;
+
+  return hash % size;
 }
 
 HashSet *init(size_t size) {
@@ -100,14 +96,12 @@ HashSet *init(size_t size) {
   return set;
 }
 
-void insert(HashSet *set, char *word) {
-  size_t index = hash(word, set->size);
+void insert(HashSet *set, int *arr, int size) {
+  size_t index = hash(arr, size, set->size);
   if (set->Entries[index] != NULL) {
     Entry *entry = (Entry *)malloc(sizeof(Entry));
-    char *string = (char *)malloc(sizeof(char) * (strlen(word) + 1));
-    strcpy(string, word);
-    entry->word = string;
     entry->next = NULL;
+    entry->arr = arr;
     Entry *next = set->Entries[index];
     while (next->next != NULL) {
       next = next->next;
@@ -115,41 +109,30 @@ void insert(HashSet *set, char *word) {
     next->next = entry;
   } else {
     Entry *entry = (Entry *)malloc(sizeof(Entry));
-    char *string = (char *)malloc(sizeof(char) * (strlen(word) + 1));
-    strcpy(string, word);
-    entry->word = string;
+    entry->arr = arr;
     entry->next = NULL;
     set->Entries[index] = entry;
   }
 }
 
-char *get(HashSet *set, char *word) {
-  size_t index = hash(word, set->size);
-  if (set->Entries[index] != NULL) {
-    if (!strcmp(word, set->Entries[index]->word)) {
-      return set->Entries[index]->word;
-    } else {
-      Entry *entry = set->Entries[index]->next;
-      while (entry != NULL) {
-        if (!strcmp(word, entry->word))
-          return entry->word;
-        entry = entry->next;
-      }
-    }
-  } else {
-    return NULL;
+int arr_equal(int *arr1, int *arr2, int size) {
+  for (int i = 0; i < size; i++) {
+    if (arr1[i] != arr2[i])
+      return 0;
   }
+  return 1;
 }
 
-int is_member(HashSet *set, char *word) {
-  size_t index = hash(word, set->size);
+int is_member(HashSet *set, int *arr, int size) {
+  size_t index = hash(arr, size, set->size);
   if (set->Entries[index] != NULL) {
-    if (!strcmp(word, set->Entries[index]->word)) {
+    if (set->Entries[index]->arr != NULL &&
+        arr_equal(arr, set->Entries[index]->arr, size)) {
       return 1;
     } else {
       Entry *entry = set->Entries[index]->next;
       while (entry != NULL) {
-        if (!strcmp(word, entry->word))
+        if (arr_equal(arr, entry->arr, size))
           return 1;
         entry = entry->next;
       }
@@ -180,7 +163,7 @@ void swap(int *board, int i, int j) {
 HashSet *set = NULL;
 HashSet *set2 = NULL;
 Queue *queue = NULL;
-char *goal = NULL;
+int *goal = NULL;
 
 Node **yield_node(Node *node) {
   Node **nodes = (Node **)malloc(sizeof(Node *) * 4);
@@ -191,105 +174,61 @@ Node **yield_node(Node *node) {
         if (row == 0) {
           nodes[0] = NULL;
         } else {
-          Node* tmp0 = (Node *)malloc(sizeof(Node));
+          Node *tmp0 = (Node *)malloc(sizeof(Node));
           tmp0->move = node->board[(row - 1) * node->size + column];
           tmp0->size = node->size;
-          tmp0->board =
-              (int *)malloc(sizeof(int) * node->size * node->size);
+          tmp0->board = (int *)malloc(sizeof(int) * node->size * node->size);
           memcpy(tmp0->board, node->board,
                  sizeof(int) * node->size * node->size);
-          tmp0->board_str = (char *)malloc(sizeof(char) * 100);
           swap(tmp0->board, row * node->size + column,
                (row - 1) * node->size + column);
           tmp0->parent = node;
-		  clock_t _begin = clock();
-          int pos = 0;
-          for (int i = 0; i < node->size * node->size; i++) {
-            pos += sprintf(tmp0->board_str + pos, "%d", tmp0->board[i]);
-          }
-          tmp0->board_str[pos] = '\0';
-		//   printf("%s\n", tmp0->board_str);
-		  begin += (double)(clock() - _begin) / CLOCKS_PER_SEC;
-		  nodes[0] = tmp0;
+          nodes[0] = tmp0;
         }
         // Down
         if (row == node->size - 1) {
           nodes[1] = NULL;
         } else {
-          Node* tmp1 = (Node *)malloc(sizeof(Node));
+          Node *tmp1 = (Node *)malloc(sizeof(Node));
           tmp1->move = node->board[(row + 1) * node->size + column];
           tmp1->size = node->size;
-          tmp1->board =
-              (int *)malloc(sizeof(int) * node->size * node->size);
+          tmp1->board = (int *)malloc(sizeof(int) * node->size * node->size);
           memcpy(tmp1->board, node->board,
                  sizeof(int) * node->size * node->size);
           tmp1->parent = node;
-          tmp1->board_str = (char *)malloc(sizeof(char) * 100);
-          tmp1->board_str[node->size * node->size] = '\0';
           swap(tmp1->board, row * node->size + column,
                (row + 1) * node->size + column);
-			clock_t _begin = clock();
-          int tracker = 0;
-          int pos = 0;
-          for (int i = 0; i < node->size * node->size; i++) {
-            pos += sprintf(tmp1->board_str + pos, "%d", tmp1->board[i]);
-          }
-          tmp1->board_str[pos] = '\0';
-		//   printf("%s\n", tmp1->board_str);
-		  begin += (double)(clock() - _begin) / CLOCKS_PER_SEC;
-		  nodes[1] = tmp1;
+          nodes[1] = tmp1;
         }
         // Left
         if (column == 0) {
           nodes[2] = NULL;
         } else {
-          Node* tmp2 = (Node *)malloc(sizeof(Node));
+          Node *tmp2 = (Node *)malloc(sizeof(Node));
           tmp2->move = node->board[row * node->size + column - 1];
           tmp2->size = node->size;
-          tmp2->board =
-              (int *)malloc(sizeof(int) * node->size * node->size);
+          tmp2->board = (int *)malloc(sizeof(int) * node->size * node->size);
           memcpy(tmp2->board, node->board,
                  sizeof(int) * node->size * node->size);
-          tmp2->board_str = (char *)malloc(sizeof(char) * 100);
-          tmp2->board_str[node->size * node->size] = '\0';
           swap(tmp2->board, row * node->size + column,
                row * node->size + column - 1);
           tmp2->parent = node;
-		  clock_t _begin = clock();
-          int pos = 0;
-          for (int i = 0; i < node->size * node->size; i++) {
-            pos += sprintf(tmp2->board_str + pos, "%d", tmp2->board[i]);
-          }
-          tmp2->board_str[pos] = '\0';
-		//   printf("%s\n", tmp2->board_str);
-		  begin += (double)(clock() - _begin) / CLOCKS_PER_SEC;
-		  nodes[2] = tmp2;
+          nodes[2] = tmp2;
         }
         // Right
         if (column == node->size - 1) {
           nodes[3] = NULL;
         } else {
-          Node* tmp3 = (Node *)malloc(sizeof(Node));
+          Node *tmp3 = (Node *)malloc(sizeof(Node));
           tmp3->move = node->board[row * node->size + column + 1];
           tmp3->size = node->size;
-          tmp3->board =
-              (int *)malloc(sizeof(int) * node->size * node->size);
+          tmp3->board = (int *)malloc(sizeof(int) * node->size * node->size);
           memcpy(tmp3->board, node->board,
                  sizeof(int) * node->size * node->size);
-          tmp3->board_str = (char *)malloc(sizeof(char) * 100);
-          tmp3->board_str[node->size * node->size] = '\0';
           swap(tmp3->board, row * node->size + column,
                row * node->size + column + 1);
           tmp3->parent = node;
-		  clock_t _begin = clock();
-		  
-          int pos = 0;
-          for (int i = 0; i < node->size * node->size; i++) {
-            pos += sprintf(tmp3->board_str + pos, "%d", tmp3->board[i]);
-          }
-          tmp3->board_str[pos] = '\0';
-		  begin += (double)(clock() - _begin) / CLOCKS_PER_SEC;
-		  nodes[3] = tmp3;
+          nodes[3] = tmp3;
         }
         if (nodes[0] == NULL && nodes[1] == NULL && nodes[2] == NULL &&
             nodes[3] == NULL) {
@@ -307,14 +246,15 @@ Node **yield_node(Node *node) {
 int number = 0;
 char *build_graph(Node *root) {
   enqueue(queue, root);
-  insert(set2, root->board_str);
+  insert(set2, root->board, root->size * root->size);
   while (!is_empty(queue)) {
-	// if (number++ > 1) {
-	// 	return NULL;
-	// }
+    // number++;
+    // if (number++ > 5) {
+    // 	return NULL;
+    // }
     // printf("%d\n", number++);
     Node *node = dequeue(queue);
-    if (!strcmp(node->board_str, goal)) {
+    if (arr_equal(node->board, goal, node->size * node->size)) {
       Node *tmp = node;
       int length = 0;
       while (tmp != NULL) {
@@ -342,25 +282,17 @@ char *build_graph(Node *root) {
       return result;
     }
     Node **nodes = yield_node(node);
-	// free(node->board);
-	// free(node->board_str);
-	// free(node);
     if (nodes == NULL) {
       continue;
     }
     for (int i = 0; i < 4; i++) {
       if (nodes[i] != NULL) {
-		// printBoard(nodes[i]->board, nodes[i]->size);
-		// printf("%s\n", nodes[i]->board_str);
-          if (is_member(set2, nodes[i]->board_str)) {
-            // free(nodes[i]->board);
-            // free(nodes[i]->board_str);
-            // free(nodes[i]);
-            continue;
-          } else {
-            enqueue(queue, nodes[i]);
-            insert(set2, nodes[i]->board_str);
-          }
+        if (is_member(set2, nodes[i]->board, nodes[i]->size * nodes[i]->size)) {
+          continue;
+        } else {
+          enqueue(queue, nodes[i]);
+          insert(set2, nodes[i]->board, nodes[i]->size * nodes[i]->size);
+        }
       }
     }
     free(nodes);
@@ -401,17 +333,6 @@ int main(int argc, char **argv) {
   // Assuming that I have a function to print the board, print it here to make
   // sure I read the input board properly for DEBUG purposes
   fclose(fp_in);
-  goal = (char *)malloc(sizeof(char) * 100);
-  int tracker = 0;
-  for (int i = 1; i < k * k; i++) {
-    char num[5];
-    sprintf(num, "%d", i);
-    for (int j = 0; j < strlen(num); j++) {
-      goal[tracker++] = num[j];
-    }
-  }
-  goal[tracker++] = '0';
-  goal[tracker] = '\0';
   ////////////////////
   // do the rest to solve the puzzle
   ////////////////////
@@ -419,24 +340,17 @@ int main(int argc, char **argv) {
   root->size = k;
   root->board = (int *)malloc(sizeof(int) * k * k);
   memcpy(root->board, initial_board, sizeof(int) * k * k);
-  root->board_str = (char *)malloc(sizeof(char) * (k * k + 1));
-  for (int i = 0; i < k * k; i++) {
-    char num[5];
-    sprintf(num, "%d", root->board[i]);
-  }
-  root->board_str[k * k] = '\0';
   root->parent = NULL;
   root->move = -1;
-  set = init(300000);
-  set2 = init(300000);
-  queue = init_queue(300000);
-  printBoard(root->board, root->size);
-  clock_t begin2 = clock();
+  set = init(200000);
+  set2 = init(200000);
+  queue = init_queue(200000);
+  goal = (int *)malloc(sizeof(int) * k * k);
+  for (int i = 1; i < k * k; i++) {
+    goal[i - 1] = i;
+  }
+  goal[k * k - 1] = 0;
   char *moves = build_graph(root);
-  clock_t end = clock();
-  double time_spent = (double)(end - begin2) / CLOCKS_PER_SEC;
-  printf("Time spent: %f\n", time_spent);
-  printf("Time spent yield node: %f\n", begin);
   // once you are done, you can use the code similar to the one below to print
   // the output into file if the puzzle is NOT solvable use something as follows
   fprintf(fp_out, "#moves\n");
@@ -444,7 +358,6 @@ int main(int argc, char **argv) {
     fprintf(fp_out, "%s\n", moves);
   else
     fprintf(fp_out, "no solution\n");
-
   // if it is solvable, then use something as follows:
   // probably within a loop, or however you stored proper moves, print them one
   // by one by leaving a space between moves, as below
